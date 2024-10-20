1b1e5b242162275024bf112531b6e3144ca2ff88
Brady Stohler <bradystohler15@gmail.com>
2024-10-20 14:59:11 -0400

    Add project files.

---
+++ x-and-os/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace x_and_os
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            clrButton = new Button();
+            saveButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
+            pictureBox1.Location = new Point(11, 20);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1692, 675);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+            // 
+            // clrButton
+            // 
+            clrButton.Location = new Point(1038, 729);
+            clrButton.Name = "clrButton";
+            clrButton.Size = new Size(202, 67);
+            clrButton.TabIndex = 1;
+            clrButton.Text = "Clear";
+            clrButton.UseVisualStyleBackColor = true;
+            clrButton.Click += btnClear_Click;
+            // 
+            // saveButton
+            // 
+            saveButton.Location = new Point(1273, 729);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(202, 67);
+            saveButton.TabIndex = 2;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += btnSave_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1715, 817);
+            Controls.Add(saveButton);
+            Controls.Add(clrButton);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button clrButton;
+        private Button saveButton;
+    }
+}
---
+++ x-and-os/Form1.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace x_and_os
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bitmap;
+        private bool drawing = false;
+        private Point lastPoint;
+        private bool rightButtonDown = false; // Flag to track right button state
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize the bitmap
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(Color.White); // Start with a white background
+            }
+            pictureBox1.Image = bitmap;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load the image from a file or URL
+            using (var webClient = new System.Net.WebClient())
+            {
+                var imageBytes = webClient.DownloadData("https://7363726.fs1.hubspotusercontent-na1.net/hub/7363726/hubfs/Content%20Offerings/COLLEGE%2016x9%20REDZONE.png?width=1200&length=1200&name=COLLEGE%2016x9%20REDZONE.png");
+                using (var ms = new System.IO.MemoryStream(imageBytes))
+                {
+                    var loadedImage = Image.FromStream(ms);
+                    // Create a new bitmap with the loaded image dimensions
+                    bitmap = new Bitmap(loadedImage.Width, loadedImage.Height);
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.DrawImage(loadedImage, new Rectangle(0, 0, loadedImage.Width, loadedImage.Height));
+                    }
+                }
+            }
+            pictureBox1.Image = bitmap;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize; // Adjust size mode as needed
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Start drawing mode and set the last point
+                drawing = true;
+                lastPoint = e.Location; // Set last point for painting
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                DrawO(e.Location); // Draw "O" immediately on right-click
+                pictureBox1.Invalidate(); // Refresh to show the new "O"
+                rightButtonDown = true; // Set flag for right button hold
+            }
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing) // If left button is held down, paint "X"
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.DrawLine(Pens.Red, lastPoint, e.Location); // Draw "X" line while holding left mouse button
+                }
+                lastPoint = e.Location;
+                pictureBox1.Invalidate(); // Refresh the PictureBox to show the new line
+            }
+            else if (rightButtonDown) // If right button is held down, draw "O"s
+            {
+                DrawO(e.Location);
+                pictureBox1.Invalidate(); // Refresh the PictureBox to show the new "O"
+            }
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+                // Check if the last point is valid for placing an "X"
+                if (!drawing) // Only place an "X" if we weren't drawing
+                {
+                    DrawX(lastPoint); // Draw "X" at the last point
+                    pictureBox1.Invalidate(); // Refresh to show the new "X"
+                }
+                drawing = false; // Stop drawing on mouse release
+
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                rightButtonDown = false; // Reset the flag on mouse release
+            }
+        }
+
+
+
+
+        private void DrawX(Point location)
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                int size = 20; // Size of the "X"
+                g.DrawLine(Pens.Red, location.X - size, location.Y - size, location.X + size, location.Y + size);
+                g.DrawLine(Pens.Red, location.X + size, location.Y - size, location.X - size, location.Y + size);
+            }
+        }
+
+        private void DrawO(Point location)
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                int size = 20; // Radius of the "O"
+                g.DrawEllipse(Pens.Blue, location.X - size, location.Y - size, size * 2, size * 2);
+            }
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "PNG Image|*.png|JPEG Image|*.jpg|Bitmap Image|*.bmp";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    bitmap.Save(sfd.FileName);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(Color.White); // Clear the bitmap with white color
+            }
+            Form1_Load(sender, e);
+            pictureBox1.Invalidate(); // Refresh the PictureBox
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            bitmap.Dispose();
+            base.OnFormClosing(e);
+        }
+    }
+}
